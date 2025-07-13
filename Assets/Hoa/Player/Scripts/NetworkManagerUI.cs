538a095c73aa19735cde8c72401e0fa4854c1ecd
GAMELOFT\hoa.tranbinh <hoa.tranbinh@gameloft.com>
2025-07-13 13:31:14 +0700

    - add connection UI

--- Assets/Hoa/Player/Scripts/NetworkManagerUI.cs
+++ Assets/Hoa/Player/Scripts/NetworkManagerUI.cs
@@ -1,17 +1,65 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Unity.Netcode;
+using PurrNet;
+using PurrNet.Transports;
+using TMPro;
 
 public class NetworkManagerUI : MonoBehaviour
 {
     [SerializeField] private Button serverButton;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+    [SerializeField] private TMP_InputField ipAddressInputField;
+    [SerializeField] private TMP_InputField port;
+    [SerializeField]
+    private UDPTransport uDPTransport;
+    [SerializeField] private GameObject networkUI;
+    [SerializeField] private GameObject mainMenuUI;
 
     private void Awake()
     {
-        serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
-        hostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
-        clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+        mainMenuUI.SetActive(false);
+        networkUI.SetActive(true);
+        // serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+        hostButton.onClick.AddListener(() => StartHost());
+        clientButton.onClick.AddListener(() => StartClient());
+    }
+
+    void StartHost()
+    {
+        uDPTransport.address = ipAddressInputField.text;
+        bool portResult = ushort.TryParse(port.text, out ushort result);
+        if (portResult && ipAddressInputField.text != "")
+        {
+            uDPTransport.serverPort = result;  // Set the port to match the server's
+            NetworkManager.main.onNetworkStarted += (NetworkManager manager, bool asServer) =>
+            {
+                Debug.Log("Network started as " + (asServer ? "server" : "client"));
+                HideNetworkUI();
+            };
+            NetworkManager.main.StartHost();
+        }
+    }
+
+    void StartClient()
+    {
+        uDPTransport.address = ipAddressInputField.text;
+        bool portResult = ushort.TryParse(port.text, out ushort result);
+        if (portResult && ipAddressInputField.text != "")
+        {
+            uDPTransport.serverPort = result;  // Set the port to match the server's
+            NetworkManager.main.onNetworkStarted += (NetworkManager manager, bool asServer) =>
+            {
+                Debug.Log("Network started as " + (asServer ? "server" : "client"));
+                HideNetworkUI();
+            };
+            NetworkManager.main.StartClient();
+        }
+    }
+
+    void HideNetworkUI()
+    {
+        networkUI.SetActive(false);
+        mainMenuUI.SetActive(true);
     }
 }
